f9526786d3af2e1b63161cce38db9e370e63dc2f
DeveloperKGH <developerkgh@gmail.com>
2021-06-10 15:58:55 +0900

    push to master

---
+++ src/main/java/dkgh/websocketmessenger/WebsocketMessengerApplication.java
@@ -0,0 +1,13 @@
+package dkgh.websocketmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dkgh/websocketmessenger/WebsocketMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package dkgh.websocketmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
