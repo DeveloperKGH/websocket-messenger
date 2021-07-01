0cc2cf7b4c4087f44ba2785d95617d876579cf8e
DeveloperKGH <developerkgh@gmail.com>
2021-07-01 13:54:17 +0900

    웹소켓 구현

--- src/main/java/dkgh/websocketmessenger/controllers/ChatController.java
+++ src/main/java/dkgh/websocketmessenger/controllers/ChatController.java
@@ -1,16 +1,32 @@
 package dkgh.websocketmessenger.controllers;
 
 import dkgh.websocketmessenger.domains.Message;
+import dkgh.websocketmessenger.domains.MessageType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
 
-    @MessageMapping("/send")
-    @SendTo("/topic/public")
-    public Message send(Message message) {
+    @PostMapping("/add-user")
+    @SendTo("/send")
+    public Message addUser(@RequestBody String nickname) {
+        System.out.println("nickname = " + nickname);
+        return Message.builder()
+                .messageType(MessageType.ENTER)
+                .sender("관리자")
+                .content(nickname + "입장 !!")
+                .build();
+    }
+
+    @MessageMapping("/receive")
+    @SendTo("/send")
+    public Message sendChatting(Message message) {
+        System.out.println("message = " + message);
+        message.setMessageType(MessageType.CHAT);
         return message;
     }
 
